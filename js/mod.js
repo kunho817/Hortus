26d078f0300e5d79e6cc917b0d603ff821911ac4
BMO Liper Okbul <kunho817@naver.com>
2023-10-23 18:42:31 +0900

    changelog update

--- js/mod.js
+++ js/mod.js
@@ -1,26 +1,25 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "Hortus",
+	id: "hortus",
+	author: "Aizure",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
-	discordName: "",
+	discordName: "Aizure",
 	discordLink: "",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	offlineLimit: 24,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Blooming Tenebris",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- Added First Layer, "Tenebris"`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
